b8065b6a8c173bff82c4ab6b45ec8215d67cf60f
Tseg-tech <tseganehamare@gmail.com>
2025-09-16 14:24:29 +0300

    modified Avatar with chakra

--- src/componnents/Avatar.js
+++ src/componnents/Avatar.js
@@ -1,10 +1,26 @@
 // styles
-import './Avatar.css'
-
+//import './Avatar.css'
+import { Box, Img } from '@chakra-ui/react'
 export default function Avatar({ src }) {
   return (
-    <div className="avatar">
-      <img src={src} alt="user avatar" />
-    </div>
+    <Box
+      sx={{
+        display: "inline-block",
+        width: "50px",
+        height: "50px",
+        borderRadius: "50%",
+        overflow: "hidden"
+      }}
+    >
+      <Img
+        src={src}
+        alt="user avatar"
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover"
+        }}
+      />
+    </Box>
   )
 }
