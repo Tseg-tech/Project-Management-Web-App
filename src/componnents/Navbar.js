bbe0fc12e5dc7e36b04a169e2cf2126f3fbeb01c
Tseg-tech <tseganehamare@gmail.com>
2025-09-22 10:17:13 +0300

    modified navbar

--- src/componnents/Navbar.js
+++ src/componnents/Navbar.js
@@ -14,16 +14,15 @@ import {
   Separator,
   Text,
   Flex,
-  Spacer,
+  // Spacer,
   Group,
   Heading,
   List,
   ListItem,
   Link as ChakraLink,
   useDisclosure,
-  Image, IconButton
-  
-
+  Image, IconButton,
+  // AbsoluteCenter,
 } from '@chakra-ui/react';
 
 import HamburgerIconImg from '../assets/hamburger-menu.svg'; // or .png/.jpg
@@ -31,7 +30,8 @@ import HamburgerIconImg from '../assets/hamburger-menu.svg'; // or .png/.jpg
 //import { DecorativeBox } from "compositions/lib/decorative-box"
 
 import { SiAnalogue } from "react-icons/si"
-
+import DashboardIcon from '../assets/dashboard_icon.svg'
+import AddIcon from '../assets/add_icon.svg'
 export default function Navbar() {
   const { logout, isPending } = useLogout()
   const { user } = useAuthContext()
@@ -42,19 +42,23 @@ export default function Navbar() {
   const { isOpen, onToggle } = useDisclosure();
 
   return (
-  //<Stack direction={{ base: "column", md: "row" }} gap="10">
+    //<Stack direction={{ base: "column", md: "row" }} gap="10">
+    //  <AbsoluteCenter> 
     <Box
       as="nav"
-      // bg="gray.50"
-      // boxShadow="md"
-      // px={4}
-      // py={2}
-       >
+      bg="gray.50"
+      boxShadow="md"
+      px={4}
+      py={2}
+      // width="100%"
+      // padding="1rem"
+
+    >
 
       <List >
         {!user && (
           <>
-            <Flex gap="4" justify="space-between">
+            <Flex gap="4" justifyContent="Center">
               <Box p="4">
                 <ChakraLink
                   as={RouterLink}
@@ -93,7 +97,7 @@ export default function Navbar() {
                   variant="ghost"
                 /> */}
               </Box>
-              <Spacer />
+              {/* <Spacer /> */}
               <Box p="4" >
                 <HStack >
                   <Text>
@@ -138,13 +142,45 @@ export default function Navbar() {
 
           <Flex>
             <Box p="4">
-              <HStack>
-                <Heading><SiAnalogue color="red" /> </Heading>
-                <Box as="span" fontWeight="bold">DMP</Box>
-              </HStack>
-            </Box>
-            <Spacer />
-            <Spacer />
+                <ChakraLink
+                  as={RouterLink}
+                  to="/welcomeboard"
+                  _focus={{ boxShadow: "none" }}
+                  cursor="pointer"
+                  transition="all 0.2s"
+                  _hover={{
+                    textDecoration: "none", // Prevent default anchor underline
+                  }}
+
+                >
+                  <HStack >
+                    <Heading><SiAnalogue color="red" /> </Heading>
+                    <Box
+                      as="span"
+                      borderBottom={isActive("/welcomeboard") ? "2px solid green" : "none"}
+                      _hover={{
+                        color: "blue.500",
+                        fontWeight: "normal",
+                        textDecoration: "none"
+                        //borderBottom: "2px solid blue" // Custom hover underline
+                      }} // custom underline
+
+                      paddingBottom="2px" fontWeight="bold"
+                    >
+                      DMP
+                    </Box>
+                  </HStack>
+                </ChakraLink>
+                {/* <IconButton
+                  aria-label="Toggle Menu"
+                  icon={isOpen ? <CloseIcon /> : <HamburgerIconImg />}
+                  display={{ base: "block", md: "none" }}
+                  onClick={onToggle}
+                  variant="ghost"
+                /> */}
+              </Box>
+            {/* <Spacer /> */}
+            {/* <Spacer /> */}
             <Box p="4" >
               <ListItem>
                 <HStack>
@@ -177,10 +213,9 @@ export default function Navbar() {
               </ListItem>
             </Box>
           </Flex>
-
         )}
       </List>
     </Box>
-   // </Stack>
+
   )
 }
